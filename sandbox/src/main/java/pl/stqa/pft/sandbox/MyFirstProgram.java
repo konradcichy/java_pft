eeb2bc06aa654e2658961b3abe0b498699850446
Konrad <konrad.cichy@gmail.com>
2017-05-20 20:09:52 +0200

    refactor

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,8 +12,9 @@ public class MyFirstProgram {
     double a = 4;
     double b = 6;
 
+
     System.out.println("Area of square : " + "len " + l + " = " + area(l));
-    System.out.println("Area of rectangle : " + "width " + a + " and " +  "length " + b + " = " + area(a,b));
+    System.out.println("Area of rectangle : " + "width " + a + " and " + "length " + b + " = " + area(a, b));
   }
 
   public static void hello(String somebody) {
@@ -28,7 +29,7 @@ public class MyFirstProgram {
     return len * len;
   }
 
-  public static double area (double a, double b){
+  public static double area(double a, double b) {
 
     return a * b;
   }
