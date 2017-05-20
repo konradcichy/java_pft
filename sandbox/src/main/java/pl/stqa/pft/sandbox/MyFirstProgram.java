42f28fb5d2ec3e336b27accddfe259611f5e646e
Konrad <konrad.cichy@gmail.com>
2017-05-20 19:26:26 +0200

    added useful functions

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,34 @@ package pl.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("konrad");
+    hello("user");
+    hello("Alexei");
 
-    System.out.println("Hello, world");
+    double l = 5;
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Area of square : " + "len " + l + " = " + area(l));
+    System.out.println("Area of rectangle : " + "width " + a + " and " +  "length " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello " + somebody + " ! ");
+
+  }
+
+
+  public static double area(double len) {
+
+    return len * len;
+  }
+
+  public static double area (double a, double b){
+
+    return a * b;
   }
 
 }
