1302573451ec95143bdf2e320e7ab45cfa258788
Konrad <konrad.cichy@gmail.com>
2017-05-20 16:57:58 +0200

    changed code format settings

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,10 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world");
-    }
+  public static void main(String[] args) {
+
+    System.out.println("Hello, world");
+  }
 
 }
 
