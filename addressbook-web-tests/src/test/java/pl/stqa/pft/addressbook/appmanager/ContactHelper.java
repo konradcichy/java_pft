8a89191d79a793c5dda5b4866ec17e2e10b11dbd
Konrad <konrad.cichy@gmail.com>
2017-06-09 13:54:31 +0200

    createContact method refactoring
    createContact method refactoring

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -47,8 +47,8 @@ public class ContactHelper extends HelperBase {
     }
   }
 
-  public void createContact(ContactData contact, boolean creation) {
-    fillContactForm(contact, creation);
+  public void createContact(ContactData contact) {
+    fillContactForm(contact, true);
     submitContactCreation();
   }
 
