1b188440ca652b2d66885d8a00aed1972474a829
Konrad <konrad.cichy@gmail.com>
2017-06-15 11:48:10 +0200

    list size comparison mechanism has been added
    list size comparison mechanism has been added

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupEditTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupEditTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Konrad on 03/06/2017.
  */
@@ -12,17 +14,17 @@ public class GroupEditTests extends TestBase {
   @Test
   public void testGroupEdit() {
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isGroupExist()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().returnToGroupPage();
-    app.getGroupHelper().selectGroup(before -1);
+    app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "newtest2", "newtest3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
