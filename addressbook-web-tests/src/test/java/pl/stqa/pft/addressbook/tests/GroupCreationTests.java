4a0acbd85f26908bdb12d363e6ad675248ba1fab
Konrad <konrad.cichy@gmail.com>
2017-06-15 17:29:19 +0200

    Using lambdas instead of old type max id calculation
    Using lambdas instead of old type max id calculation

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("nameofgroup", null, null);
+    GroupData group = new GroupData("nameofgroup2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
@@ -25,7 +25,7 @@ public class GroupCreationTests extends TestBase {
       }
     }
 
-    group.setId(max);
+    group.setId(after.stream().max(((o1, o2) -> Integer.compare(o1.getId(), o2.getId()))).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupEditTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupEditTests.java
@@ -15,22 +15,23 @@ public class GroupEditTests extends TestBase {
   @Test
   public void testGroupEdit() {
     app.getNavigationHelper().goToGroupPage();
-    if (! app.getGroupHelper().isGroupExist()) {
+    if (!app.getGroupHelper().isGroupExist()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().returnToGroupPage();
-    app.getGroupHelper().selectGroup(before.size() -1);
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1", "newtest2", "newtest3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "newtest2", "newtest3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() -1);
+
+    before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
