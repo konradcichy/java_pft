7457bf63f383426246393f901e93b70f6c76273d
Konrad <konrad.cichy@gmail.com>
2017-07-06 11:34:26 +0200

    task no 12 completed

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
@@ -14,6 +14,57 @@ public class ContactData {
   private String email2;
   private String email3;
   private String allEmails;
+  private String group;
+
+  public ContactData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public ContactData withFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public ContactData withLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public ContactData withAddress(String address) {
+    this.address = address;
+    return this;
+  }
+
+  public ContactData withHomePhone(String homePhone) {
+    this.homePhone = homePhone;
+    return this;
+  }
+
+
+  public String getAllPhones() {
+    return allPhones;
+  }
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
+
+  public ContactData withMobilePhone(String mobilePhone) {
+    this.mobilePhone = mobilePhone;
+    return this;
+  }
+
+  public ContactData withWorkPhone(String workPhone) {
+    this.workPhone = workPhone;
+    return this;
+  }
+
+  public ContactData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
 
   public String getEmail2() {
     return email2;
@@ -42,60 +93,6 @@ public class ContactData {
     this.allEmails = allEmails;
     return this;
 
-  }
-
-  private String group;
-
-
-  public String getAllPhones() {
-    return allPhones;
-  }
-
-  public ContactData withAllPhones(String allPhones) {
-    this.allPhones = allPhones;
-    return this;
-  }
-
-
-  public ContactData withFirstName(String firstName) {
-    this.firstName = firstName;
-    return this;
-  }
-
-  public ContactData withId(int id) {
-    this.id = id;
-    return this;
-  }
-
-
-  public ContactData withLastName(String lastName) {
-    this.lastName = lastName;
-    return this;
-  }
-
-  public ContactData withAddress(String address) {
-    this.address = address;
-    return this;
-  }
-
-  public ContactData withHomePhone(String homePhone) {
-    this.homePhone = homePhone;
-    return this;
-  }
-
-  public ContactData withMobilePhone(String mobilePhone) {
-    this.mobilePhone = mobilePhone;
-    return this;
-  }
-
-  public ContactData withWorkPhone(String workPhone) {
-    this.workPhone = workPhone;
-    return this;
-  }
-
-  public ContactData withEmail(String email) {
-    this.email = email;
-    return this;
   }
 
   @Override
