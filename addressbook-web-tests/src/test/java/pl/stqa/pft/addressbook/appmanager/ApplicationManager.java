b0d6c5fa11a7763f6c013ba27a3a01c24193f6af
Konrad <konrad.cichy@gmail.com>
2017-07-09 13:24:22 +0200

    getting properties from property file created
    * possibility to change browser while running tests from command line

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -9,7 +9,10 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,6 +20,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
+  private final Properties properties;
   WebDriver wd;
 
   private NavigationHelper navigationHelper;
@@ -26,11 +30,15 @@ public class ApplicationManager {
   private String browser;
 
   public ApplicationManager(String browser) {
+
     this.browser = browser;
+    properties = new Properties();
   }
 
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties,target"))));
 
     if (Objects.equals(browser, BrowserType.FIREFOX)) {
       FirefoxBinary binary = new FirefoxBinary(new File("/Applications/FirefoxESR.app/Contents/MacOS/firefox-bin"));
@@ -44,12 +52,12 @@ public class ApplicationManager {
 
 
     wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("secret", "admin");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
 
