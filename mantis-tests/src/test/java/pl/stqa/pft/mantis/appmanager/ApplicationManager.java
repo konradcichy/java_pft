accf691c477af752d80ce458a770e4aa83bd368a
Konrad <konrad.cichy@gmail.com>
2017-07-17 18:41:07 +0200

    new module for mantis tests added

---
+++ mantis-tests/src/test/java/pl/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,61 @@
+package pl.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Konrad on 31/05/2017.
+ */
+public class ApplicationManager {
+
+  private final Properties properties;
+  WebDriver wd;
+
+  private String browser;
+
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+    if (Objects.equals(browser, BrowserType.FIREFOX)) {
+      FirefoxBinary binary = new FirefoxBinary(new File("/Applications/FirefoxESR.app/Contents/MacOS/firefox-bin"));
+      FirefoxProfile profile = new FirefoxProfile();
+      wd = new FirefoxDriver(binary, profile);
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
+      wd = new InternetExplorerDriver();
+    }
+
+
+    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+}
---
+++ mantis-tests/src/test/java/pl/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package pl.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.BeforeSuite;
+import pl.stqa.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by Konrad on 31/05/2017.
+ */
+public class TestBase {
+
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+
+  }
+
+
+}
+
+
