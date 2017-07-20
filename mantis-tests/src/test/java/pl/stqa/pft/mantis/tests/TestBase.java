f5665f37fec4ddf3c35c34c28ceeefc788021e13
Konrad <konrad.cichy@gmail.com>
2017-07-20 12:23:55 +0200

    ftp connection configured
    * ftp connection configured
    * config_inc.php file added to project
    * HelperBase moved

--- mantis-tests/src/test/java/pl/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/pl/stqa/pft/mantis/tests/TestBase.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Konrad on 31/05/2017.
  */
@@ -16,7 +19,15 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"),"config_inc.php", "config_inc.php.bak");
 
+  }
+
+
+  @BeforeSuite(alwaysRun = true)
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.bak", "config_inc.php");
+    app.stop();
   }
 
 
